fc26862ab3751ba1b9d4cd7bc4cc0e836c37a622
LEE Hyeon Seung <wila@tistory.com>
2020-06-25 11:32:06 +0900

    3.2. Component Life Cycle

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import React from "react";
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    console.log("hello");
+  }
   state = {
     count: 0,
   };
+  componentDidMount() {
+    console.log("component rendered");
+  }
+  componentDidUpdate() {
+    console.log("I just updated.");
+  }
   render() {
+    console.log("I am render");
     return (
       <div>
         <h1>The number is: {this.state.count}</h1>
